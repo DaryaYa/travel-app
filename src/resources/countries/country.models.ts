7c575fe2bea6fd3402381ad48d09e853271fdb7d
Yury Barysevich <yury_barysevich1@epam.com>
2021-03-10 01:52:29 +0300

    feat:add model country

---
+++ src/resources/countries/country.models.ts
@@ -0,0 +1,39 @@
+import { Schema, model, Types } from 'mongoose';
+
+const userSchema = new Schema({
+  nameEN: String,
+  nameRU: String,
+  capitalEN: String,
+  capitalRU: String,
+  regionEN: String,
+  regionRU: String,
+  population: Number,
+  latlng: [Number],
+  area: Number,
+  capitalTimezone: String,
+  bordersEN: [String],
+  bordersRU: [String],
+  currencies: [
+    {
+      code: String,
+      nameEN: String,
+      nameRU: String,
+      symbol: String,
+    },
+  ],
+  language: [{ nameEN: String, nameRU: String, nativeName: String }],
+  flag: String,
+  photo: String,
+  video: String,
+  attractions: [
+    {
+      attractionNameEN: String,
+      attractionNameRU: String,
+      description: String,
+      descriptionRU: String,
+      photo: String,
+      link: String,
+    },
+  ],
+  stars: [{ user: { type: Types.ObjectId, ref: 'User' }, countStar: Number }],
+});
