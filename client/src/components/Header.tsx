c5d40308b43ee96e285ec761ac7f350f3c16b31e
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2021-03-09 11:31:55 +0300

    feat: i react-bootstrap & i react-icons & create Header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BiLogIn } from 'react-icons/bi';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='primary' expand="md">
+        <Navbar.Brand href="/">LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Form inline className="m-2">
+            <FormControl type="text" placeholder="Search" />
+            <Button variant="success"><AiOutlineSearch /></Button>
+          </Form>
+          <Form inline className="m-2">
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Control as="select" custom >
+                <option selected>EN</option>
+                <option>RU</option>
+                <option>AM</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+          <Button variant="light" className="m-2">
+            <BiLogIn style={{fontSize: '1.3rem'}} />
+          </Button>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
