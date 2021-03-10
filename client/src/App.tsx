2ed115294f5fac8676ea30bf4dc95a912db1f817
DaryaYa <lyalyaka@mail.ru>
2021-03-11 00:16:24 +0300

    feat: add clock % weather

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import { Widget } from './components/Widget';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Widget lat={15} lng={50} timeZone={"Europe/Moscow"} language={"en-GB"} />
     </>
   );
 }
