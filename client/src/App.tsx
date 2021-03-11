9bfb08472cd12f47f72b5e0cf1b6990b10eb77ec
DaryaYa <lyalyaka@mail.ru>
2021-03-11 21:55:23 +0300

    rebase backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
 import Country from './pages/Country/Country';
 import Main from './pages/Main/Main';
-=======
 import Header from './components/Header';
 import { Widget } from './components/Widget';
->>>>>>> 2ed1152... feat: add clock % weather
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
       <Switch>
         <Route path={'/main'} component={Main} exact />
         <Route path={'/country'} component={Country} exact />
         <Route path={'/auth'} component={Auth} exact />
         <Redirect to={'/main'} />
       </Switch>
-=======
       <Header />
       <Widget lat={15} lng={50} timeZone={"Europe/Moscow"} language={"en-GB"} />
->>>>>>> 2ed1152... feat: add clock % weather
     </>
   );
 };
