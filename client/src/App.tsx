e433c51ef22b3544f0323f0f96ee0c1558337fe1
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2021-03-12 21:56:06 +0300

    feat: leave rating function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Rating from './components/Rating';
+import LeaveRatingForm from './components/LeaveRatingForm';
 import Footer from './components/Footer';
 
 const App = () => {
-  // HARDCODE
-  const RenderRating: React.FC = () => {
-    const attractionRating = [
-      {
-        stars: 1,
-        numReviews: 3,
-      },
-      {
-        stars: 1.5,
-        numReviews: 12,
-      },
-      {
-        stars: 3,
-        numReviews: 6,
-      },
-      {
-        stars: 2,
-        numReviews: 9,
-      },
-      {
-        stars: 5,
-        numReviews: 54,
-      }
-    ];
+  // HARDCODE START
+  const [reviewsArr, setReviewsArr] = useState([1, 3]);
+  const [rating, setRating] = 
+    useState( reviewsArr.reduce((acc, cur) => acc + cur) / reviewsArr.length );
 
-    return (
-      <div>
-        {attractionRating.map(el => (
-          <Rating value={el.stars} text={`${el.numReviews} reviews`} />
-        ))}
-      </div>
-    )
+  useEffect(() => {
+    setRating(reviewsArr.reduce((acc, cur) => acc + cur) / reviewsArr.length);
+  }, [reviewsArr])
+
+  const onChangeHandler= (val: number) => {
+    setReviewsArr([...reviewsArr, val]);
   }
+
+  const RenderRating: React.FC = () => (
+    <>
+      <Rating value={rating} text={`${reviewsArr.length} reviews`} />
+    </>
+  )
+  // HARDCODE END
 
   return (
     <>
       <Header />
       <main>
         <RenderRating />
+        <LeaveRatingForm
+          onChangeHandler={onChangeHandler}
+        />
       </main>
       <Footer />
     </>
