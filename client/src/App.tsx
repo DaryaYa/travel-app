f24300c8cc1adcea05cf3d1cedab00c160343688
Yury Barysevich <yury_barysevich1@epam.com>
2021-03-17 00:01:52 +0300

    feat: show errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Auth from './pages/Auth/Auth';
 import Country from './pages/Country/Country';
 import Main from './pages/Main/Main';
 import { useTypesSelector } from './components/hooks/useTypedSelector';
 
 const App = () => {
+  const notify = () => toast('Wow so easy!');
+  const { error } = useTypesSelector(state => state.user);
+  if (error) {
+    notify();
+    console.log(1);
+  }
   return (
     <>
       <Switch>
@@ -14,6 +23,8 @@ const App = () => {
         <Route path={'/auth'} component={Auth} exact />
         <Redirect to={'/main'} />
       </Switch>
+
+      <ToastContainer />
     </>
   );
 };
