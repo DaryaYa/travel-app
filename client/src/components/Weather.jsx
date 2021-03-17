73466f043b2a136e82fbfeb2da740ff4638b4493
DaryaYa <lyalyaka@mail.ru>
2021-03-17 22:10:49 +0300

    fix: lang weather and clock

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -2,77 +2,82 @@ import React, { useState, useEffect } from 'react';
 import { FaTemperatureHigh } from 'react-icons/fa';
 import { BsDropletHalf } from 'react-icons/bs';
 import { GiWindsock, GiPerson } from 'react-icons/gi';
+import { useTranslation } from 'react-i18next'; // trans
 
 const REACT_APP_OPEN_WEATHER = '985407983380c5d99fa1bb48a8e0eec0';
 
-export const Weather = ({city, language}) => {
+export const Weather = ({ city, language }) => {
+  const { t, i18n } = useTranslation(); // trans
 
-    const [weather, setWeather] = useState({
-      temp: 0,
-      humidity: 0,
-      feels_like: 0,
-      description: " ",
-      iconCode: "",
-      wind: 0,
-    });
+  const [weather, setWeather] = useState({
+    temp: 0,
+    humidity: 0,
+    feels_like: 0,
+    description: ' ',
+    iconCode: '',
+    wind: 0,
+  });
 
-    useEffect(() => {
+  // const [lang, setLang] = useState(language);
 
-       const fetchData = async () => {
-      
-           const res = fetch(
-             `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${language}&units=metric&APPID=${REACT_APP_OPEN_WEATHER}`,
-           );
-           const data1 = await res;
-           const data2 = await data1.json();
-           const data = data2.list[0];
+  // useEffect(()=> {
+  //   setLang(localStorage.getItem('i18nextLng'));
+  // }, [lang])
 
-        setWeather({
-          temp: data.main.temp,
-          humidity: data.main.humidity,
-          feels_like: data.main.feels_like,
-          description: data.weather[0].description,
-          iconCode: data.weather[0].icon,
-          wind: data.wind.speed,
-        });
- 
-        return data;
-       } 
-       fetchData();
-    },[city, language])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${language}&units=metric&APPID=${REACT_APP_OPEN_WEATHER}`,
+      );
+      const data1 = await res;
+      const data2 = await data1.json();
+      const data = data2.list[0];
 
-     const url='https://openweathermap.org/img/wn/'
+      setWeather({
+        temp: data.main.temp,
+        humidity: data.main.humidity,
+        feels_like: data.main.feels_like,
+        description: data.weather[0].description,
+        iconCode: data.weather[0].icon,
+        wind: data.wind.speed,
+      });
 
-    return (
-      <div className="weather d-inline-flex flex-column justify-content-center">
-        <img
-          alt="weather icon"
-          className="mx-auto d-block"
-          src={`${url}${weather.iconCode}@2x.png`}
-        />
+      return data;
+    };
+    fetchData();
+  }, [city, language]);
 
-        <div className="weather-parameter description">{weather.description}</div>
+  const url = 'https://openweathermap.org/img/wn/';
 
-        <div className="weather-parameter temperature">
-          <FaTemperatureHigh />
-          <span className="text">{Math.round(weather.temp)}°C</span>
-        </div>
+  return (
+    <div className="weather d-inline-flex flex-column justify-content-center">
+      <img
+        alt="weather icon"
+        className="mx-auto d-block"
+        src={`${url}${weather.iconCode}@2x.png`}
+      />
 
-        <div className="weather-parameter humidity">
-          <BsDropletHalf />
-          <span className="text">{weather.humidity}%</span>
-        </div>
+      <div className="weather-parameter description">{weather.description}</div>
 
-        <div className="weather-parameter feels-like">
-          <GiPerson />
-          <span className="text">{Math.round(weather.feels_like)}°C</span>
-        </div>
-
-        <div className="weather-parameter wind-speed">
-          <GiWindsock />
-          <span className="text">{Math.round(weather.wind)}m/s</span>
-        </div>
+      <div className="weather-parameter temperature">
+        <FaTemperatureHigh />
+        <span className="text">{Math.round(weather.temp)}°C</span>
       </div>
-    );
-    
-}
+
+      <div className="weather-parameter humidity">
+        <BsDropletHalf />
+        <span className="text">{weather.humidity}%</span>
+      </div>
+
+      <div className="weather-parameter feels-like">
+        <GiPerson />
+        <span className="text">{Math.round(weather.feels_like)}°C</span>
+      </div>
+
+      <div className="weather-parameter wind-speed">
+        <GiWindsock />
+        <span className="text">{Math.round(weather.wind)}{t('Weather.1')}</span>
+      </div>
+    </div>
+  );
+};
